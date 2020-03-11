ec52054421290d5c5e39f52254dd831d606d67df
Szymon Rodziewicz <dev.jaca@gmail.com>
2020-03-11 14:40:16 +0100

    Work on lightweight server

--- src/core/leightweightserver.scala
+++ src/core/leightweightserver.scala
@@ -5,13 +5,74 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver._
 import java.io.OutputStream
 import java.net.URI
+import java.io.ByteArrayOutputStream
+import scala.collection.JavaConverters._
 
-object LeightweightRequestHandler {
-  val handler: HttpHandler = ???
+object LeightweightServer {
+
+  case class RequestQuery(query: String, parameters: Map[String, List[String]]) 
+
+  def readBody(exchange: HttpExchange): Array[Byte] = {
+    val is = exchange.getRequestBody()
+    val output = new ByteArrayOutputStream();
+    val buffer = Array.ofDim[Byte](2048)
+    var len = is.read(buffer)
+    while(len != 0) {
+      output.write(buffer, 0, len)
+      is.read(buffer)
+    }
+    output.toByteArray()
+  }
+
+  def readQuery(exchange: HttpExchange): RequestQuery = {
+    val uri = exchange.getRequestURI()
+    val query = uri.getQuery()
+    val paramStrings = query.split("&")
+    val params = paramStrings.foldLeft(Map[String, List[String]]()) { (map, elem) =>
+      val split = elem.split("=")
+      val key = split(0)
+      val value = split(1)
+      val values = map.getOrElse(key, List.empty[String])
+      map + (key -> (value::values))
+    }
+    RequestQuery(query, params)
+  }
+
+  def readHeaders(exchange: HttpExchange): Map[String, String] = {
+    exchange.getRequestHeaders().asScala.mapValues(_.get(0)).toMap
+  }
+
+  def mapToRequest(exchange: HttpExchange): Request = {
+    val body = readBody(exchange)
+    val query = readQuery(exchange)
+    val headers = readHeaders(exchange)
+    Request(
+      Method.from(exchange.getRequestMethod()),
+      exchange.getRequestHeaders().getFirst("ContentType"),
+      body.length,
+      body,
+      query.query,
+      false,
+      exchange.getLocalAddress().getHostName(),
+      exchange.getLocalAddress().getPort(),
+      exchange.getHttpContext().getPath(),
+      headers,
+      query.parameters
+    )
+  }
 }
 
-abstract class LeightweightRequestHandler(port: Int, context: String) extends RequestHandler {
-    val httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-    httpServer.createContext(context, ???)
+abstract class LeightweightServer(port: Int, context: String) extends RequestHandler {
+
+    val httpServer = HttpServer.create(new InetSocketAddress(port), 0)
+
+    val handler: HttpHandler = { (exchange: HttpExchange) =>
+
+    }
+
+    httpServer.createContext(context, handler)
+    httpServer.setExecutor(null)
+    httpServer.start()
+    
 }
 
